a6cf5281533b6ee969485264bb3b97cb6bf692ca
emamyari <emamyari@yahoo.com>
2024-09-17 20:00:51 +0330

    add counter logic

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { useState,useEffect } from 'react'
 export const Header = (props) => {
+let [count,setCount]=useState(0)  
+useEffect(() => {
+    let c=0
+    props.sabad.forEach(element => {
 
+      c=c+element.count
+      setCount(c)
+    });
+  },[props.sabad]);
 
+  
   return (
     <div  className='main m'  >products count :
      <button onClick={()=>props.openModal()}>
-         {props.sabad.length}
+         {count}
      </button>
      </div>
   )
